175fb7cf4f608da465bc4f582fb0731efddadb5b
Nithish <nithishpini@gmail.com>
2026-01-26 13:54:12 +0530

    Unnnessacary files removed and logo replaced

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,16 +1,9 @@
 import appointment_img from "./appointment_img.png";
 import header_img from "./header_img.png";
 import group_profiles from "./group_profiles.png";
-import profile_pic from "./profile_pic.png";
 import contact_image from "./contact_image.png";
 import about_image from "./about_image.png";
-import logo from "./logo.svg";
-import dropdown_icon from "./dropdown_icon.svg";
-import chats_icon from "./chats_icon.svg";
 import verified_icon from "./verified_icon.svg";
-import arrow_icon from "./arrow_icon.svg";
-import info_icon from "./info_icon.svg";
-import upload_icon from "./upload_icon.png";
 import stripe_logo from "./stripe_logo.png";
 import razorpay_logo from "./razorpay_logo.png";
 import mental_health_therapy from "./mental_health_therapy.png";
@@ -25,17 +18,10 @@ export const assets = {
   appointment_img,
   header_img,
   group_profiles,
-  logo,
   healspacelogo,
-  chats_icon,
   verified_icon,
-  info_icon,
-  profile_pic,
-  arrow_icon,
   contact_image,
   about_image,
-  dropdown_icon,
-  upload_icon,
   stripe_logo,
   razorpay_logo,
 };
